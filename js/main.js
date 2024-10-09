9f6de2808835af029d9bf707db1c302dfa1832af
Martina Uccheddu <martina.uccheddu@studio.unibo.it>
2024-10-09 16:07:24 +0200

    files from the project

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//EXPANDABLE GRID
+// Hide all elements with class="containerTab", except for the one that matches the clickable grid column
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
+//HORIZONTAL TABS
+function openCity(evt, cityName) {
+				  // Declare all variables
+				  var i, tabcontent, tablinks;
+
+				  // Get all elements with class="tabcontent" and hide them
+				  tabcontent = document.getElementsByClassName("tabcontent");
+				  for (i = 0; i < tabcontent.length; i++) {
+				    tabcontent[i].style.display = "none";
+				  }
+
+				  // Get all elements with class="tablinks" and remove the class "active"
+				  tablinks = document.getElementsByClassName("tablinks");
+				  for (i = 0; i < tablinks.length; i++) {
+				    tablinks[i].className = tablinks[i].className.replace(" active", "");
+				  }
+
+				  // Show the current tab, and add an "active" class to the button that opened the tab
+				  document.getElementById(cityName).style.display = "block";
+				  evt.currentTarget.className += " active";
+}
+
